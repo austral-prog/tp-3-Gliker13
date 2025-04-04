62654436ebeb1bb6ba58ce02a93bd399a4300f3d
Tomas Fernandez Narice <tfernandeznarice@mail.austral.edu.ar>
2025-04-04 11:28:52 -0300

    hand in

--- in_string.py
+++ in_string.py
@@ -1,6 +1,11 @@
 def check_vowels():
     # Código a implementar utilizando input.
-
-
+    name = input().lower()
+    print("contiene a:","a" in name)
+    print("contiene e:","e" in name)
+    print("contiene i:","i" in name)
+    print("contiene o:","o" in name)
+    print("contiene u:","u" in name)
+check_vowels()
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
--- slice_simple.py
+++ slice_simple.py
@@ -1,6 +1,9 @@
 def slice_simple():
     texto = "Awesome"
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
+    print(texto[0:3].lower())
+    print(texto[4:6].lower())
+    print(texto[0:3].lower())
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
 
